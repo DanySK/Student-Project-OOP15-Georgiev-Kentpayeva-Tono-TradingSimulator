e77836ef45b969409fd3fdb67ca6fdabbb5d14cb
robertotono1995 <robertotono1995@gmail.com>
2016-02-29 18:45:52 +0100

    avviata la view per la piattaforma di trading

---
+++ src/trdingMVC/viewPlatform.java
@@ -0,0 +1,5 @@
+package trdingMVC;
+
+public interface viewPlatform {
+
+}
---
+++ src/trdingMVC/viewPlatformImpl.java
@@ -0,0 +1,17 @@
+package trdingMVC;
+
+public class viewPlatformImpl implements viewPlatform {
+
+	@Override
+	public void GeneraGrafo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void DatiUser() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
