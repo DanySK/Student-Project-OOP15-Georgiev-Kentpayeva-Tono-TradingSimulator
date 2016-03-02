c243b1472e267a3b96ef9058b1944b374b07d425
robertotono1995 <robertotono1995@gmail.com>
2016-03-02 18:39:52 +0100

    aggiunto package per gli Indicatori Tecnici

---
+++ src/IndicatoriTecnici/MedieMobiliModel.java
@@ -0,0 +1,6 @@
+package IndicatoriTecnici;
+
+public interface MedieMobiliModel {
+	public void CalcoloMediaMobilSemplice();
+	public void CalcoloMediaMobilEsponenziale();
+}
---
+++ src/IndicatoriTecnici/MedieMobiliModelImpl.java
@@ -0,0 +1,17 @@
+package IndicatoriTecnici;
+
+public class MedieMobiliModelImpl implements MedieMobiliModel {
+
+	@Override
+	public void CalcoloMediaMobilSemplice() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void CalcoloMediaMobilEsponenziale() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
