497c39e7a3473abf60c4dca5fe1652e53e0a91dd
gio777 <gio777@localhost>
2016-04-30 21:10:40 +0200

    up model

--- src/modelPlatform/ModelPlatformImpl.java
+++ src/modelPlatform/ModelPlatformImpl.java
@@ -11,24 +11,27 @@ import org.jfree.data.time.Millisecond;
 import org.jfree.data.time.ohlc.OHLCSeries;
 import org.jfree.data.time.ohlc.OHLCSeriesCollection;
 
+import viewPlatform.ViewPlatformImpl;
+
 public class ModelPlatformImpl implements ModelPlatform {
 	
 	
-	
+	public boolean isUpDateModel=false;
 
-	
+
+	String csvFile = "datasrc/DAT_MT_EURUSD_M1_201602.csv";
+	BufferedReader br = null;
+	String line = "";
+	//List<ValuesAssetImpl> value=new ArrayList<>();
+	OHLCSeries asset=new OHLCSeries("EUR/USD");
+	OHLCSeriesCollection dataset=new OHLCSeriesCollection();
+
 
 	@Override
 	public OHLCSeriesCollection dataFeed() {
 		// TODO Auto-generated method stub
 		
-		String csvFile = "datasrc/DAT_MT_EURUSD_M1_201602.csv";
-		BufferedReader br = null;
-		String line = "";
-		//List<ValuesAssetImpl> value=new ArrayList<>();
-		OHLCSeries asset=new OHLCSeries("EUR/USD");
-		OHLCSeriesCollection dataset=new OHLCSeriesCollection();
-
+		
 		try {
 
 			br = new BufferedReader(new FileReader(csvFile));
@@ -37,7 +40,20 @@ public class ModelPlatformImpl implements ModelPlatform {
 			       
 				String[] quote = line.split(",");
 				//value.add(new ValuesAssetImpl(Double.parseDouble(quote[2]),Double.parseDouble(quote[3]),Double.parseDouble(quote[4]),Double.parseDouble(quote[5]),Double.parseDouble(quote[6])));
-				asset.add(new Millisecond(),Double.parseDouble(quote[2]),Double.parseDouble(quote[3]),Double.parseDouble(quote[4]),Double.parseDouble(quote[5]));
+				//asset.add(new Millisecond(),Double.parseDouble(quote[2]),Double.parseDouble(quote[3]),Double.parseDouble(quote[4]),Double.parseDouble(quote[5]));
+				
+				//asset.add(new Millisecond(),20,6,8,10);
+				
+				if(isUpDateModel==true){
+					System.out.println("bene3");
+					asset.add(new Millisecond(),Double.parseDouble(quote[2]),Double.parseDouble(quote[3]),Double.parseDouble(quote[4]),Double.parseDouble(quote[5]));
+					
+				}
+				else{
+					System.out.println("male3");
+					
+				}
+				
 				
 				dataset.addSeries(asset);
 
