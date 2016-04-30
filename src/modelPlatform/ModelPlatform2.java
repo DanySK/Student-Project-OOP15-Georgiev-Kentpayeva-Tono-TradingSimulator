256d0583f68293700966d13f3df8af3e02f29c24
gio777 <gio777@localhost>
2016-04-30 12:00:41 +0200

    new up

---
+++ src/modelPlatform/ModelPlatform2.java
@@ -0,0 +1,19 @@
+package modelPlatform;
+
+
+
+import org.jfree.data.time.ohlc.OHLCSeriesCollection;
+
+public interface ModelPlatform2 {
+	//prova
+	public OHLCSeriesCollection dataFeed();
+	
+	
+	
+	
+	
+
+}
+
+
+
