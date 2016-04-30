6d30cf6ba061f977b5226b8223bef30aee29f0af
tonoR95 <tonoR95@localhost>
2016-04-30 19:13:28 +0200

    update view to sync MVC

---
+++ src/viewPlatform/Graph.java
@@ -0,0 +1,14 @@
+package viewPlatform;
+
+import java.awt.event.ActionEvent;
+
+public interface Graph {
+	
+	boolean isUpDate=false;
+	
+	public void actionPerformed(final ActionEvent e);
+	
+	public void update();
+
+	
+}
