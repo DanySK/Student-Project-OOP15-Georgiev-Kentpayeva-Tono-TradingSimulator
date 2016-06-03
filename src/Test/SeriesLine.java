b56320f833d8d042e3bf965de1ef41669e2eec5d
tonoR95 <tonoR95@localhost>
2016-06-03 17:11:25 +0200

    added classes Test

---
+++ src/Test/SeriesLine.java
@@ -0,0 +1,15 @@
+package Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SeriesLine {
+
+	@org.junit.Test
+	public void test() {		
+		
+		
+	}
+}
+
