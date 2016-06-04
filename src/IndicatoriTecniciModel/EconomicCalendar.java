f39789ccfac0a2ee2ff177ad85182aaacc5c582d
madina kentpayeva <madina.kentpayeva@studio.unibo.it>
2016-06-04 20:12:25 +0200

    fix calendar model

--- src/IndicatoriTecniciModel/EconomicCalendar.java
+++ src/IndicatoriTecniciModel/EconomicCalendar.java
@@ -25,63 +25,83 @@ public class EconomicCalendar {
 	BufferedReader br = null;
 	List<String> list;
 	int count=0;
+	boolean fine;
 	
 	//1)Data+(Mese)	2)Ora legale	3)Importanza	4)Paese		5)Evento	
 	//6)Attuale	7)Previsione	8)Precedente	
 	
 	public void data() {
 		// TODO Auto-generated method stub
-		
-		
-		try {
- 			br = new BufferedReader(new FileReader("datasrc/econimicCalendarInformation.csv"));
- 		} catch (FileNotFoundException e1) {
- 			// TODO Auto-generated catch block
- 			e1.printStackTrace();
- 		}
-         String input;
-         try {
- 			br.readLine();
- 		} catch (IOException e) {
- 			// TODO Auto-generated catch block
- 			e.printStackTrace();
- 		}
-         try {
-        	 
-        	 
- 			   while((input = br.readLine())!=null&&count==0)
- 			   {
- 				   this.list.add(input);
- 			   }
- 			    if(count<list.size()){
-	 			    count++;
-	 			    
-	 			    StringTokenizer s = new StringTokenizer(list.get(count-1), ";");
-	 			    
-	 			    s.nextToken();
+		  
+			try {
+	 			br = new BufferedReader(new FileReader("datasrc/econimicCalendarInformation.csv"));
+	 		} catch (FileNotFoundException e1) {
+	 			// TODO Auto-generated catch block
+	 			e1.printStackTrace();
+	 		}
+	         String input;
+	         
+	      		  
+	         try {
+	 			br.readLine();
+	 		} catch (IOException e) {
+	 			// TODO Auto-generated catch block
+	 			e.printStackTrace();
+	 		}
+	         try {
+	        	 
+	        	 
+	 			   while((input = br.readLine())!=null)
+	 			   {
+	 				   this.list.add(input);
+	 			   }
 	 			   
-	 			    double open=Double.parseDouble(s.nextToken());
-	 			    double high=Double.parseDouble(s.nextToken());
-	 			    double low=Double.parseDouble(s.nextToken());
-	 			    double close=Double.parseDouble(s.nextToken());
 	 			  
-	 			    System.out.println("ec" +open+" "+high+" "+low+" "+close);
-	 			    //System.out.flush();
+	 			   
+	 			    while(count<list.size()){
+		 			    count++;
+		 			    
+		 			    StringTokenizer s = new StringTokenizer(list.get(count-1), ",");
+		 			    
+		 			    //s.nextToken();
+		 			
+		 			    //1)Data+(Mese)	2)Ora legale	3)Importanza	4)Paese		5)Evento	
+		 				//6)Attuale	7)Previsione	8)Precedente	
+		 				
+		 			    String data=s.nextToken(),
+		 			    		ora=s.nextToken(),
+		 			    		importanza=s.nextToken(),
+		 			    		paese=s.nextToken(),
+		 			    		evento=s.nextToken(),
+		 			    		attuale=s.nextToken(),
+		 			    		previone=s.nextToken(),
+		 			    		precedente=s.nextToken(),
+		 			    		mese=s.nextToken();
+				 			    		
+		 			    		
+		 			   System.out.println(data+
+		 			    		ora+
+		 			    		importanza+
+		 			    		paese+
+		 			    		evento+
+		 			    		attuale+
+		 			    		previone+
+		 			    		precedente+
+		 			    		mese);
+		 			    
+	 			    }
 	 			    
- 			    }
- 			    else{
- 			    	//lancio errori
- 			    }
- 			   
- 			
- 		} catch (NumberFormatException e) {
- 			// TODO Auto-generated catch block
- 			e.printStackTrace();
- 		} catch (IOException e) {
- 			// TODO Auto-generated catch block
- 			e.printStackTrace();
- 		}
+	 			   
+	 			
+	 		} catch (NumberFormatException e) {
+	 			// TODO Auto-generated catch block
+	 			e.printStackTrace();
+	 		} catch (IOException e) {
+	 			// TODO Auto-generated catch block
+	 			e.printStackTrace();
+	 		}
+		}
 		
-	}
+	
 
 }
